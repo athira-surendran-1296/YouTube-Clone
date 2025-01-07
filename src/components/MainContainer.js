06890416b56b1c1b51357631b5f6980a22ba1e66
athira <athirasurendran1296@gmail.com>
2025-01-07 11:19:04 +0530

    Refactored Main Container

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ButtonList from './ButtonList'
 import VideoContainer from './VideoContainer'
-import { useSelector } from 'react-redux';
 import useMostPopularVideos from '../utils/hooks/useMostPopularVideos';
 
 const MainContainer = () => {
-  const isSideNavOpen = useSelector(store => store.appConfig.isSideNavOpen);
 
   useMostPopularVideos();
 
